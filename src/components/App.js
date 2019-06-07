a3504b3a2e0e3ffe1fc8eb13c7a4e6b4d894fb99
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-07 14:22:43 +0800

    Add App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,77 @@
 import React from "react";
-import logo from "../assets/logo.svg";
+import Input from "./Input";
+import Button from "./Button";
+import ToDoList from "./ToDoList";
 import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Can Remove Item(Bonus): No
+// Able to handle very long todo item(Bonus): No
+
+// Responsive(Bonus): No
+// Prevent adding empty task(Bonus): Yes
+// Cannot have two of the same items (strings)
+
+// UI Usability Review:
+// Add style to increase the size of the todo list so that it makes better use of the white space.
+// Keep the list items and the input field aligned to be more tidy.
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ["buy milk", "eat dinner", "nail javascript", "give feedback"],
+      searchField: ""
+    };
+  }
+
+  searchField = event => {
+    this.setState({
+      searchField: event.target.value
+    });
+  };
+
+  handleClick = () => {
+    if (this.state.searchField !== "") {
+      const items = [...this.state.items];
+      items.push(this.state.searchField);
+
+      this.setState({
+        items
+      });
+    }
+  };
+
+  handleKeyDown = event => {
+    const enterCondition =
+      event.key === "Enter" && this.state.searchField !== "";
+    if (enterCondition) {
+      const items = [...this.state.items];
+      items.push(this.state.searchField);
+
+      this.setState({
+        items
+      });
+      event.target.value = "";
+    }
+  };
+
+  /* toDoList.addEventListener("click", event => {
+    event.target.classList.toggle("done");
+  }); */
+
+  render() {
+    return (
+      <div className="app">
+        <h1>My To-Do List</h1>
+        <Input
+          searchField={this.searchField}
+          handleKeyDown={this.handleKeyDown}
+        />
+        <Button handleClick={this.handleClick} />
+        <ToDoList items={this.state.items} />
+      </div>
+    );
+  }
 }
 
 export default App;
