a5cc670edfbf7ddcd8960a736d284971a273f59c
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-08 13:56:08 +0800

    Add and comment out React Dnd add drag and drop interaction code

--- src/components/ToDoItems.js
+++ src/components/ToDoItems.js
@@ -1,13 +1,26 @@
 import React from "react";
+// import { useDrag } from "react-dnd";
 
-const ToDoItems = ({ items }) => {
+function ToDoItems({ items }) {
+  // const [{ isDragging }, drag] = useDrag({
+  //   item: { type: "LIST_ITEMS" },
+  //   collect: monitor => ({
+  //     isDragging: !!monitor.isDragging()
+  //   })
+  // });
+
   return items.map(item => {
     return (
-      <li className="todo-items" key={item}>
+      <li
+        // ref={drag}
+        // style={{ opacity: isDragging ? 0.5 : 1 }}
+        className="todo-item"
+        key={item}
+      >
         {item}
       </li>
     );
   });
-};
+}
 
 export default ToDoItems;
