7af1bb5e5132bd7144e3319b8e34f199caaea432
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-11 07:04:39 +0800

    Add placeholder to Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,6 +4,7 @@ const Input = ({ searchField, handleKeyDown }) => {
   return (
     <input
       type="text"
+      placeholder="Enter your item"
       className="todo-input"
       onChange={searchField}
       onKeyDown={handleKeyDown}
