18d976f7221bbea7e31e343952a52636dfe40302
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-10 14:29:06 +0800

    Add Credit component for flaticon cross icon

---
+++ src/components/Credit.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Credit = () => {
+  return (
+    <div>
+      Icons made by{" "}
+      <a href="https://www.flaticon.com/authors/hadrien" title="Hadrien">
+        Hadrien
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>{" "}
+      is licensed by{" "}
+      <a
+        href="http://creativecommons.org/licenses/by/3.0/"
+        title="Creative Commons BY 3.0"
+        target="_blank"
+      >
+        CC 3.0 BY
+      </a>
+    </div>
+  );
+};
+
+export default Credit;
