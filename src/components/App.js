03513cd2ce06ec4d577dd1da07f43c546eba3137
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-12 16:56:30 +0800

    Add children items in each week

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,42 @@ class App extends React.Component {
       items: [
         {
           id: "1",
-          text: "week 1",
+          text: "Week 1",
           children: [
-            { id: "1-1", text: "git bash", children: [] },
-            { id: "1-2", text: "jest", children: [] }
+            { id: "1-1", text: "Git bash", children: [] },
+            { id: "1-2", text: "Jest", children: [] }
           ]
         },
-        { id: "2", text: "week 2", children: [] },
-        { id: "3", text: "week 3", children: [] },
-        { id: "4", text: "week 4", children: [] },
+        {
+          id: "2",
+          text: "Week 2",
+          children: [
+            { id: "2-1", text: "this, ES6, error handling", children: [] },
+            {
+              id: "2-2",
+              text: "Promise, async/await, testing with Jest",
+              children: []
+            }
+          ]
+        },
+        {
+          id: "3",
+          text: "Week 3",
+          children: [
+            {
+              id: "3-1",
+              text:
+                "What is React?, Single Page Apps, React.createElement(), JSX, function components, class components, ReactDOM.render(), Virtual DOM",
+              children: []
+            },
+            {
+              id: "3-2",
+              text: "Promise, async/await, testing with Jest",
+              children: []
+            }
+          ]
+        },
+        { id: "4", text: "Week 4", children: [] },
         {
           id: "5",
           text: "A veryyyyy longgggggggggggggggggggg word testing123",
