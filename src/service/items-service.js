786f8d1afc82645d69f66464481be05aeb278445
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-16 10:45:46 +0800

    Add display: false to each item's state

--- src/service/items-service.js
+++ src/service/items-service.js
@@ -3,21 +3,29 @@ const data = [
     id: "1",
     text: "Week 1",
     children: [
-      { id: "1-1", text: "Git bash", children: [] },
-      { id: "1-2", text: "Jest", children: [] }
-    ]
+      { id: "1-1", text: "Git bash", children: [], display: false },
+      { id: "1-2", text: "Jest", children: [], display: false }
+    ],
+    display: false
   },
   {
     id: "2",
     text: "Week 2",
     children: [
-      { id: "2-1", text: "this, ES6, error handling", children: [] },
+      {
+        id: "2-1",
+        text: "this, ES6, error handling",
+        children: [],
+        display: false
+      },
       {
         id: "2-2",
         text: "Promise, async/await, testing with Jest",
-        children: []
+        children: [],
+        display: false
       }
-    ]
+    ],
+    display: false
   },
   {
     id: "3",
@@ -27,20 +35,24 @@ const data = [
         id: "3-1",
         text:
           "What is React?, Single Page Apps, React.createElement(), JSX, function components, class components, ReactDOM.render(), Virtual DOM",
-        children: []
+        children: [],
+        display: false
       },
       {
         id: "3-2",
         text: "Promise, async/await, testing with Jest",
-        children: []
+        children: [],
+        display: false
       }
-    ]
+    ],
+    display: false
   },
-  { id: "4", text: "Week 4", children: [] },
+  { id: "4", text: "Week 4", children: [], display: false },
   {
     id: "5",
     text: "A veryyyyy longgggggggggggggggggggg word testing123",
-    children: []
+    children: [],
+    display: false
   }
 ];
 
