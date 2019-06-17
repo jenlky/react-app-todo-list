7c6da20c707d9c7ddec2cefbef6b9f75b98d64b4
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-17 08:01:05 +0800

    Add more data to items-service.js

--- src/service/items-service.js
+++ src/service/items-service.js
@@ -3,8 +3,89 @@ const data = [
     id: "1",
     text: "Week 1",
     children: [
-      { id: "1-1", text: "Git bash", children: [], display: false },
-      { id: "1-2", text: "Jest", children: [], display: false }
+      {
+        id: "1-1",
+        text: "Git bash",
+        children: [
+          {
+            id: "1-1-1",
+            text:
+              "What's the difference between Windows powershell, bash and zsh? ",
+            children: [],
+            display: false
+          }
+        ],
+        display: false
+      },
+      { id: "1-2", text: "VSCode shortcuts", children: [], display: false },
+      {
+        id: "1-3",
+        text: "Users Groups and Others permission",
+        children: [],
+        display: false
+      },
+      {
+        id: "1-4",
+        text: "CSS",
+        children: [
+          {
+            id: "1-4-1",
+            text: "What are the CSS selectors?",
+            children: [],
+            display: false
+          },
+          {
+            id: "1-4-2",
+            text: "What are the CSS combinators?",
+            children: [
+              {
+                id: "1-4-2-1",
+                text: "Adjacent sibling combinator: A + B",
+                children: [],
+                display: false
+              },
+              {
+                id: "1-4-2-2",
+                text: "General sibling combinator: A ~ B",
+                children: [],
+                display: false
+              },
+              {
+                id: "1-4-2-3",
+                text: "Child combinator: A > B",
+                children: [],
+                display: false
+              },
+              {
+                id: "1-4-2-4",
+                text: "Descendant combinator: A B",
+                children: [],
+                display: false
+              },
+              {
+                id: "1-4-2-5",
+                text: "Column combinator: A || B",
+                children: [],
+                display: false
+              }
+            ],
+            display: false
+          },
+          {
+            id: "1-4-2",
+            text: "What are the CSS Pseudo-classes?",
+            children: [],
+            display: false
+          },
+          {
+            id: "1-4-3",
+            text: "What are the CSS Pseudo-elements?",
+            children: [],
+            display: false
+          }
+        ],
+        display: false
+      }
     ],
     display: false
   },
