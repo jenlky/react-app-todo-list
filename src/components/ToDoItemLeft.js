72039f9238c6c3f0d818272ed1a1dc852772690e
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-30 12:39:50 +0800

    Refactor ToDoItemLeft and create Plus and RightTriangle components from it

--- src/components/ToDoItemLeft.js
+++ src/components/ToDoItemLeft.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "react-tippy/dist/tippy.css";
-import { Tooltip } from "react-tippy";
+import Plus from "./Plus";
+import RightTriangle from "./RightTriangle";
 
 const ToDoItemLeft = ({ item, addChildItem, toggleDisplay }) => {
   // item.id '1-2' after split becomes ['1','2'] and passed to addChildItem
@@ -8,29 +8,12 @@ const ToDoItemLeft = ({ item, addChildItem, toggleDisplay }) => {
 
   return (
     <div className="todo-item-left-icons">
-      <Tooltip
-        title="Click to add an item below"
-        position="bottom"
-        trigger="mouseenter"
-        delay="100"
-      >
-        <span
-          className="todo-item-left-plus"
-          onClick={() => addChildItem(itemId)}
-        >
-          &#xFF0B;
-        </span>
-      </Tooltip>
-      <span
-        className={
-          item.display
-            ? "todo-item-left-triangle toggled-triangle"
-            : "todo-item-left-triangle"
-        }
-        onClick={() => toggleDisplay(itemId)}
-      >
-        &#x25b6;
-      </span>
+      <Plus id={itemId} addChildItem={addChildItem} />
+      <RightTriangle
+        display={item.display}
+        id={itemId}
+        toggleDisplay={toggleDisplay}
+      />
     </div>
   );
 };
