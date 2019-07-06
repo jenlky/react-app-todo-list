b5f97f52c0a41f1a91c791966017b6b0f8839248
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-07-06 12:08:45 +0800

    Add cors package, and mount it in app.js

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ require("./utils/db");
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const userRouter = require("./routes/user.route");
 
 app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
