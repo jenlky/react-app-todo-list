336d59af0bbb6de0c5bf310b91ad52b56d9abe9a
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-07-13 13:34:11 +0800

    Overhaul react state to look like lists in backend userData

--- client/src/service/lists-service.js
+++ client/src/service/lists-service.js
@@ -1,14 +1,12 @@
 const lists = [
   {
-    id: "1",
-    text: "Week 1",
-    children: [
+    id: 1,
+    name: "Week 1",
+    listItems: [
       {
-        id: "1-1",
         text: "Git bash",
         children: [
           {
-            id: "1-1-1",
             text:
               "What's the difference between Windows powershell, bash and zsh? ",
             children: [],
@@ -17,35 +15,29 @@ const lists = [
         ],
         display: false
       },
-      { id: "1-2", text: "VSCode shortcuts", children: [], display: false },
+      { text: "VSCode shortcuts", children: [], display: false },
       {
-        id: "1-3",
         text: "Users Groups and Others permission",
         children: [],
         display: false
       },
       {
-        id: "1-4",
         text: "CSS",
         children: [
           {
-            id: "1-4-1",
             text: "What are the CSS selectors?",
             children: [],
             display: false
           },
           {
-            id: "1-4-2",
             text: "What are the CSS combinators?",
             children: [
               {
-                id: "1-4-2-1",
                 text: "Adjacent sibling combinator: A + B",
                 children: [],
                 display: false
               },
               {
-                id: "1-4-2-2",
                 text: "General sibling combinator: A ~ B",
                 children: [],
                 display: false
@@ -54,13 +46,11 @@ const lists = [
             display: false
           },
           {
-            id: "1-4-3",
             text: "What are the CSS Pseudo-classes?",
             children: [],
             display: false
           },
           {
-            id: "1-4-4",
             text: "What are the CSS Pseudo-elements?",
             children: [],
             display: false
@@ -72,17 +62,15 @@ const lists = [
     display: false
   },
   {
-    id: "2",
-    text: "Week 2",
-    children: [
+    id: 2,
+    name: "Week 2",
+    listItems: [
       {
-        id: "2-1",
         text: "this, ES6, error handling",
         children: [],
         display: false
       },
       {
-        id: "2-2",
         text: "Promise, async/await, testing with Jest",
         children: [],
         display: false
