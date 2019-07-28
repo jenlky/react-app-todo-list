26ee1837489bf59bd3afdfbdfe458422796e5c89
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-07-28 21:36:56 +0800

    Add signup and login to api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,21 @@
 import axios from "./axios";
 
+export const signUp = async (name, username, email, password) => {
+  return await axios.post(`/signup`, {
+    name,
+    username,
+    email,
+    password
+  });
+};
+
+export const login = async (username, password) => {
+  return await axios.post(`/login`, {
+    username,
+    password
+  });
+};
+
 export const getAllLists = async username => {
   return await axios.get(`/users/${username}`);
 };
