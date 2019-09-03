4c614cb136e4aa5eac71b58911dc72142b41d8bf
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-03 12:54:26 +0800

    Create isAuthenticated js file

---
+++ server/src/utils/isAuthenticated.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const controller = require("../controllers/user.controller");
+const { verifyToken } = require("../utils/token");
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.sendStatus(401);
+    }
+
+    const token = authorization.split(" ")[1];
+    if (token) {
+      const payload = verifyToken(token);
+      const foundUser = await controller.checkPayload(payload.user);
+      if (foundUser) {
+        next();
+      } else {
+        throw new Error("User cannot be found");
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = isAuthenticated;
