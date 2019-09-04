b2a76bb11b1539df166adab135c55c853de71716
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-04 16:22:01 +0800

    Add headers to the necessary routes in api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,10 @@
 import axios from "./axios";
 
+const token = sessionStorage.getItem("jwt");
+const config = {
+  headers: { Authorization: `Bearer ${token}` }
+};
+
 export const signUp = async (name, username, email, password) => {
   return await axios.post(`/signup`, {
     name,
@@ -16,22 +21,26 @@ export const login = async (username, password) => {
   });
 };
 
+export const logout = async () => {
+  return await axios.post("/logout");
+};
+
 export const getAllLists = async username => {
-  return await axios.get(`/users/${username}`);
+  return await axios.get(`/users/${username}`, config);
 };
 
 export const createOneList = async username => {
-  return await axios.post(`/users/${username}`);
+  return await axios.post(`/users/${username}`, config);
 };
 
 export const updateOneList = async (username, id) => {
-  return await axios.put(`/users/${username}/lists/${id}`);
+  return await axios.put(`/users/${username}/lists/${id}`, config);
 };
 
 export const deleteOneList = async (username, id) => {
-  return await axios.delete(`/users/${username}/lists/${id}`);
+  return await axios.delete(`/users/${username}/lists/${id}`, config);
 };
 
 export const overwriteListItems = async (username, id) => {
-  return await axios.put(`/users/${username}/lists/${id}/items`);
+  return await axios.put(`/users/${username}/lists/${id}/items`, config);
 };
