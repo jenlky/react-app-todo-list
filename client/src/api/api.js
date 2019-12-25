6582aee0804182aa67a637a1ca6db85b9973f0c1
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-12-25 16:13:08 +0800

    Toggle url domain name depending on environment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,8 +7,13 @@ const insertAuthorization = () => {
   };
 };
 
+const domain =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : process.env.REACT_APP_URI;
+
 export const signUp = async (name, username, email, password) => {
-  return await axios.post(`/signup`, {
+  return await axios.post(`${domain}/signup`, {
     name,
     username,
     email,
@@ -17,27 +22,32 @@ export const signUp = async (name, username, email, password) => {
 };
 
 export const login = async (username, password) => {
-  return await axios.post(`/login`, {
+  console.log(process.env.NODE_ENV);
+  return await axios.post(`${domain}/login`, {
     username,
     password
   });
 };
 
 export const logout = async () => {
-  return await axios.post("/logout", insertAuthorization());
+  return await axios.post(`${domain}/logout`, insertAuthorization());
 };
 
 export const getAllLists = async username => {
-  return await axios.get(`/users/${username}`, insertAuthorization());
+  return await axios.get(`${domain}/users/${username}`, insertAuthorization());
 };
 
 export const createOneList = async username => {
-  return await axios.post(`/users/${username}`, "", insertAuthorization());
+  return await axios.post(
+    `${domain}/users/${username}`,
+    "",
+    insertAuthorization()
+  );
 };
 
 export const updateOneList = async (username, index, requestBody) => {
   return await axios.put(
-    `/users/${username}/lists/${index}`,
+    `${domain}/users/${username}/lists/${index}`,
     requestBody,
     insertAuthorization()
   );
@@ -45,14 +55,14 @@ export const updateOneList = async (username, index, requestBody) => {
 
 export const deleteOneList = async (username, index) => {
   return await axios.delete(
-    `/users/${username}/lists/${index}`,
+    `${domain}/users/${username}/lists/${index}`,
     insertAuthorization()
   );
 };
 
 export const overwriteListItems = async (username, index, requestBody) => {
   return await axios.put(
-    `/users/${username}/lists/${index}/items`,
+    `${domain}/users/${username}/lists/${index}/items`,
     requestBody,
     insertAuthorization()
   );
