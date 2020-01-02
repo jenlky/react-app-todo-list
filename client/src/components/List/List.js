a3bba6a610e58d44e8e7eef541c5e20a28e4faf9
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2020-01-02 12:33:39 +0800

    Use Material UI Tooltip instead of tippy

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,9 +1,17 @@
 import React from "react";
-import Button from "../Button";
 import ListItem from "./ListItem";
 import Cross from "./Cross";
 import OpenWithIcon from "@material-ui/icons/OpenWith";
 import CancelIcon from "@material-ui/icons/Cancel";
+import Tooltip from "@material-ui/core/Tooltip";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  tooltip: {
+    fontSize: "1.05rem",
+    backgroundColor: "black"
+  }
+}));
 
 export default function List({
   list,
@@ -17,6 +25,8 @@ export default function List({
   toggleDisplay,
   expandOrCollapseAll
 }) {
+  const classes = useStyles();
+
   return (
     <div className="list">
       <div className="list-name-row">
@@ -38,14 +48,26 @@ export default function List({
         />
       </div>
       <div>
-        <OpenWithIcon
-          className="open-with-icon"
-          onClick={e => expandOrCollapseAll(true, list.id)}
-        />
-        <CancelIcon
-          className="cancel-icon"
-          onClick={e => expandOrCollapseAll(false, list.id)}
-        />
+        <Tooltip
+          title="Expand all"
+          aria-label="expand-all"
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <OpenWithIcon
+            className="open-with-icon"
+            onClick={e => expandOrCollapseAll(true, list.id)}
+          />
+        </Tooltip>
+        <Tooltip
+          title="Collapse all"
+          aria-label="collapse-all"
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <CancelIcon
+            className="cancel-icon"
+            onClick={e => expandOrCollapseAll(false, list.id)}
+          />
+        </Tooltip>
         <input
           type="text"
           className="list-input"
@@ -54,12 +76,19 @@ export default function List({
           onChange={e => keyInItemHandler(e, list.id)}
           onKeyDown={e => handleEnter(e, list.id)}
         />
-        <Button
-          className="list-add-btn tooltip"
-          addFirstItem={addFirstItem}
-          listId={list.id}
-          dataTestId="list-add-btn"
-        />
+        <Tooltip
+          title="Add a parent item"
+          aria-label="add-parent-item"
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <button
+            className="list-add-btn tooltip"
+            onClick={e => addFirstItem(e, list.id)}
+            data-testid="list-add-btn"
+          >
+            Add
+          </button>
+        </Tooltip>
       </div>
       <ul className="list-parent">
         {list.listItems.map(listItem => {
